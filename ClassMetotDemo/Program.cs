559567cf95a3931cf476a5a66de027e92c8a0c59
İLHAN ASLAN <aslanilhan6@gmail.com>
2021-01-31 17:51:57 +0300

    23 January Interface

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "ilhan";
+            int age = 26;
+
+            Customer customer1 = new Customer();
+            customer1.CustomerName = "Jack";
+            customer1.Age = 23;
+            customer1.Id = "12345678900001";
+
+            Customer customer2 = new Customer();
+            customer2.CustomerName = "Kelly";
+            customer2.Age = 43;
+            customer2.Id = "12345678900002";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerName = "Thomas";
+            customer3.Age = 34;
+            customer3.Id = "12345678900003";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.CustomerName + " : " + customer.Age + " : " + customer.Id);
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Kampintro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type safety - tip güvenliği
+            //Do not repeat yourselt - Kendini tekrarlama
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int öğrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");            
+            }
+            else
+            {
+                Console.WriteLine("degişmedi butonu");
+            }
+            
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+
+        }
+    }
+}
